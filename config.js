959af8423291541e089316d0aec17fd1eb3c7546
ahmadfd-afk <id.localcredit@gmail.com>
2026-09-13 04:39:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 // File: config.js
 // Daftar Seluruh Database Instansi (Tanpa batas jumlah)
-const APP_INSTITUTIONS = [
+window.APP_INSTITUTIONS = [
     {
         id: "kampus_uniqhba",
         nama: "Universitas UNIQHBA",
         password_akses: "uniqhba2026", // Kode unik untuk membuka portal ini
-        database_provider: "firebase",
-        firebase: {
-            url: "https://uniqhba-f2a4e-default-rtdb.asia-southeast1.firebasedatabase.app",
-            key: "AIzaSyB6baepb5qABIqJljTOTbN80N_A43SxbT4"
+        database_provider: "supabase",
+        supabase: {
+            url: "URL_SUPABASE_UNIQHBA_DI_SINI",
+            key: "KEY_SUPABASE_UNIQHBA_DI_SINI"
         }
     },
     {
         id: "rsud_ntb",
-        nama: "Klinik RSUD Provinsi N,
+        nama: "Klinik RSUD Provinsi NTB",
         password_akses: "klinikntb123", // Kode unik untuk membuka portal ini
         database_provider: "firebase",
         firebase: {
@@ -21,5 +21,4 @@ const APP_INSTITUTIONS = [
             key: "KEY_FIREBASE_KLINIK_DI_SINI"
         }
     }
-    // Anda bisa menambahkan instansi lain di bawah ini di masa depan
 ];
